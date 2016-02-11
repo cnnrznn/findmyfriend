c8bec67285076a4e77bb11da6d4ae76c070020bd
= <=>
2016-02-10 19:09:15 -0500

    Fixed the skeleton testhost.py file

--- testhost.py
+++ testhost.py
@@ -3,8 +3,12 @@
 # This is a UDP client for testing the FindMyFriend
 # server.
 
+import socket
+
 _host = ""
-_port = 1235
+_port = 1233
+_devsport = 1235
+_locsport = 1234
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((_host, _port))
