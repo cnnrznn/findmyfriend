ab2d346a97c1e622e7f2f8e71733ed35ad6749f9
= <=>
2016-02-10 20:47:24 -0500

    Adding skeleton code for device id server

--- devidserver.py
+++ devidserver.py
@@ -4,6 +4,9 @@
 
 import socket
 
+_req_devid = -1
+_free_devid = -2
+
 _host = ""
 _port = 1235
 
@@ -15,7 +18,13 @@ sock.listen(5)
 while True:
     tmp_sock, addr = sock.accept()
 
-    # TODO receive operation (request, free)
+    # receive operation (request, free)
+    req = int(tmp_sock.recv(1))
+
     # TODO perform operation
+    if req == _req_devid:
+        pass
+    elif req == _free_devid:
+        pass
 
     tmp_sock.close()
