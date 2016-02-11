e102430b1ad47857ffa5108583cbaada83971a21
= <=>
2016-02-10 20:38:46 -0500

    Starting to expand the devid server

--- devidserver.py
+++ devidserver.py
@@ -9,3 +9,13 @@ _port = 1235
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((_host, _port))
+
+sock.listen(5)
+
+while True:
+    tmp_sock, addr = sock.accept()
+
+    # TODO receive operation (request, free)
+    # TODO perform operation
+
+    tmp_sock.close()
