63bc57b30ace8a1bb9c35ebdf5c825f1676db6a7
Connor <cnnrznn@udel.edu>
2016-02-12 21:33:58 -0500

    Progress

--- server.py
+++ server.py
@@ -23,6 +23,12 @@ _inf_port = 1235 # port for handling TCP information exchange (device id, channe
 
 loc_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 loc_sock.bind((_host, _loc_port))
+loc_sock.settimeout(0.01)
+
+inf_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+inf_sock.bind((_host, _inf_port))
+inf_sock.settimeout(0.01)
+inf_sock.listen(5)
 
 channels = dict()   # {chan_id, (dev_id1, dev_id2)}
 devs = dict()       # {dev_id, (chan_id, lat, lon)}
@@ -30,63 +36,75 @@ devs = dict()       # {dev_id, (chan_id, lat, lon)}
 next_channel = 0
 
 while True:
-    data, addr = loc_sock.recvfrom(256)
+    try:
+        tmp_sock, tmp_addr = inf_sock.accept()
+        # TODO handle communication
+    except socket.timeout:
+        # do nothing
+        pass
 
-    # extract data from packet
-    data = data.split(',')
-    req_id = int(data[0])
-    req_lat = float(data[1])
-    req_lon = float(data[2])
+    try:
+        data, addr = loc_sock.recvfrom(256)
 
-    """
-    if req_id < 0: # a request for id
-        if addr in addrs:
-            # resend assigned channel number
-            sock.sendto(addrs[addr][0], addr)
-        else:
-            # assign new channel number
-            addrs[addr] = [next_channel, -1, -1]
-            channels[next_channel] = [addr, None]
-            next_channel += 1
-    else:
-        # respond with the relevent location
-        #sock.sendto("39.677501,-75.75207", addr)
-        if addr in addrs:
-            # respond with the other addr's location
-            possaddrs = channels[req_id]
-            message = ""
-            if addr == possaddrs[0]:
-                message = construct_message(possaddrs[1])
+        # extract data from packet
+        data = data.split(',')
+        req_id = int(data[0])
+        req_lat = float(data[1])
+        req_lon = float(data[2])
+
+        """
+        if req_id < 0: # a request for id
+            if addr in addrs:
+                # resend assigned channel number
+                sock.sendto(addrs[addr][0], addr)
             else:
-                message = construct_message(possaddrs[0])
-            sock.sendto(message, addr)
-            # update this addr's location
-            addrs[addr][1] = req_lat
-            addrs[addr][2] = req_lon
-        elif channels[reqid][1] == None:
-            # TODO join this addr to the connection
-            pass
+                # assign new channel number
+                addrs[addr] = [next_channel, -1, -1]
+                channels[next_channel] = [addr, None]
+                next_channel += 1
         else:
-            # ignore request
-            pass
+            # respond with the relevent location
+            #sock.sendto("39.677501,-75.75207", addr)
+            if addr in addrs:
+                # respond with the other addr's location
+                possaddrs = channels[req_id]
+                message = ""
+                if addr == possaddrs[0]:
+                    message = construct_message(possaddrs[1])
+                else:
+                    message = construct_message(possaddrs[0])
+                sock.sendto(message, addr)
+                # update this addr's location
+                addrs[addr][1] = req_lat
+                addrs[addr][2] = req_lon
+            elif channels[reqid][1] == None:
+                # TODO join this addr to the connection
+                pass
+            else:
+                # ignore request
+                pass
 
-    # print information about request
-    print "Packet from", addr
-    data[2] = struct.unpack(">I", data[2])[0]
-    print "Data"
-    for i in xrange(3):
-        print data[i]
+        # print information about request
+        print "Packet from", addr
+        data[2] = struct.unpack(">I", data[2])[0]
+        print "Data"
+        for i in xrange(3):
+            print data[i]
 
-    print
-    """
+        print
+        """
 
-    print "Request from", addr
-    print "req_id:", req_id, type(req_id)
-    print "req_lat:", req_lat, type(req_lat)
-    print "req_lon:", req_lon, type(req_lon)
-    print
+        print "Request from", addr
+        print "req_id:", req_id, type(req_id)
+        print "req_lat:", req_lat, type(req_lat)
+        print "req_lon:", req_lon, type(req_lon)
+        print
 
-    # DEBUG print dictionaries
-    print channels
-    print devs
-    print
+        # DEBUG print dictionaries
+        print channels
+        print devs
+        print
+
+    except socket.timeout:
+        # do nothing
+        pass
