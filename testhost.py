e66c044f18170ca440b03f76455e91c004d67c4f
Connor <cnnrznn@udel.edu>
2016-02-13 12:51:26 -0500

    Updating test host program

--- testhost.py
+++ testhost.py
@@ -6,12 +6,16 @@
 import socket
 
 _host = ""
-_port = 1233
-_devsport = 1235
-_locsport = 1234
+_tcp_port = 1232
+_udp_port = 1233
+_inf_port = 1235
+_loc_port = 1234
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((_host, _port))
+udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp_sock.bind((_host, _udp_port))
+
+tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_sock.bind((_host, _tcp_port))
 
 # TODO send device requests
 # TODO send channel requests
